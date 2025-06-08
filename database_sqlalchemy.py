bdcc202c12d1d5d99621a200e2cdc5aa24f9a8b0
elmarAbdullayev <114047390+elmarAbdullayev@users.noreply.github.com>
2025-06-08 14:09:54 +0200

    mein neuer Stand

--- database_sqlalchemy.py
+++ database_sqlalchemy.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///marketplace.db"
+DATABASE_URL = "postgresql://marketplace_db_23a6_user:J08kKwtV5Aah6HYgnSBVVnjbMsxbQpAM@dpg-d12not3uibrs73fdqgv0-a.oregon-postgres.render.com/marketplace_db_23a6"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit = False ,autoflush=False,bind = engine)
-
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
