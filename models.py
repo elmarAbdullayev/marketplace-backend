e1380bc603ee86daa6e862569072e0a5cae26fd0
elmarAbdullayev <114047390+elmarAbdullayev@users.noreply.github.com>
2025-06-08 15:05:02 +0200

    mein neuer Stand

--- models.py
+++ models.py
@@ -8,14 +8,14 @@ class User(Base):
     __tablename__ = "user"
 
     ID = Column(Integer, primary_key=True, index=True) # bei SQLAlchemy automatisch aktiviert, wenn du primary_key=True bei einem Integer-Feld angibst.
-    name = Column(String(20))
+    name = Column(String(40))
     surname = Column(String(20))
     email = Column(String(60), unique=True, index=True)
     password = Column(String(100))
-    street = Column(String(30))
+    street = Column(String(50))
     street_number = Column(Integer)
     number = Column(String(20))
-    role = Column(String(10))
+    role = Column(String(20))
     created_at = Column(DateTime)
 
 # Tabelle: data
@@ -24,9 +24,9 @@ class Data(Base):
 
     ID = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("user.ID"))
-    title = Column(String(20))
+    title = Column(String(50))
     city = Column(String(20))
-    info = Column(String(40))
+    info = Column(String(250))
     category = Column(String(30))
     picture = Column(String(255))
     created_at = Column(DateTime)
